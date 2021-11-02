0d9357ca62e1d5fdc3ed65c0141a4b32aeab91b3
johnjamespj <johnjamespj@gmail.com>
2021-11-02 02:39:00 +0000

    add end variable to run.sh

--- extraction/run.sh
+++ extraction/run.sh
@@ -2,5 +2,6 @@ UNIT_COUNT=100
 DUMP=~/data/human_entities_backup.csv.bz2
 BUCKET=vg-embedding
 START=5700
+END=5800
 
-UNIT_COUNT=${UNIT_COUNT} DUMP=${DUMP} BUCKET=${BUCKET} START=${START} python3 main.py
+UNIT_COUNT=${UNIT_COUNT} DUMP=${DUMP} BUCKET=${BUCKET} START=${START} END=${END} python3 main.py
